2f3275552d5a5e9b1e1638591dd6461e3a8b8d62
Alex <112178448+Alx424@users.noreply.github.com>
2022-12-01 16:51:03 -0500

    c

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,12 +4,18 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.drive.ArcadeDrive;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.ExampleSubsystem;
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -22,8 +28,25 @@ public class RobotContainer {
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
+  //hardware
+  VictorSPX
+    l1 = new VictorSPX(RobotMap.BACK_MOTOR_LEFT),
+    l2 = new VictorSPX(RobotMap.FRONT_MOTOR_LEFT);
+  TalonSRX
+    r1 = new TalonSRX(RobotMap.BACK_MOTOR_RIGHT),
+    r2 = new TalonSRX(RobotMap.FRONT_MOTOR_RIGHT);
+  AHRS gyro = new AHRS();
+  //subsystems
+  Drivetrain drive = new Drivetrain(l1, l2, r1, r2, gyro);
+  PS4Controller PScon;
+
+  // command
+  ArcadeDrive arcadeDrive = new ArcadeDrive(drive, PScon::getLeftY, PScon::getRightX);
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    // Set as default command
+    drive.setDefaultCommand(arcadeDrive);
     // Configure the button bindings
     configureButtonBindings();
   }
